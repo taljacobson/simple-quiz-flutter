edd21c79b5153ac5d70474937e4074b8e2b02df3
taljacobson <tal1989@gmail.com>
2018-03-04 23:29:27 +0200

    create animation and dispose when done

--- lib/ui/answer_overlay.dart
+++ lib/ui/answer_overlay.dart
@@ -2,18 +2,17 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 class AnswerOverlay extends StatefulWidget {
-
+  final VoidCallback _onTap;
   final bool _isCorrect;
 
-  AnswerOverlay(this._isCorrect);
-  
+  AnswerOverlay(this._isCorrect, this._onTap);
+
   @override
   State<StatefulWidget> createState() => new AnswerOverlayState();
 }
 
-class AnswerOverlayState extends State<AnswerOverlay> with SingleTickerProviderStateMixin {
-
-  
+class AnswerOverlayState extends State<AnswerOverlay>
+    with SingleTickerProviderStateMixin {
   Animation<double> _iconAnim;
   AnimationController _iconAnimController;
 
@@ -21,10 +20,19 @@ class AnswerOverlayState extends State<AnswerOverlay> with SingleTickerProviderS
   void initState() {
     super.initState();
 
-    _iconAnimController = new AnimationController(duration: new Duration(seconds: 2), vsync: this);
-    _iconAnim = new CurvedAnimation(parent: _iconAnimController, curve: Curves.elasticOut );
+    _iconAnimController = new AnimationController(
+        duration: new Duration(seconds: 2), vsync: this);
+    _iconAnim = new CurvedAnimation(
+        parent: _iconAnimController, curve: Curves.elasticOut);
     _iconAnim.addListener(() => this.setState(() => {}));
     _iconAnimController.forward();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _iconAnimController.dispose();
   }
 
   @override
@@ -33,7 +41,7 @@ class AnswerOverlayState extends State<AnswerOverlay> with SingleTickerProviderS
     return new Material(
       color: Colors.black54,
       child: new InkWell(
-        onTap: () => print('overlay'),
+        onTap: widget._onTap,
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -43,14 +51,22 @@ class AnswerOverlayState extends State<AnswerOverlay> with SingleTickerProviderS
                 shape: BoxShape.circle,
               ),
               child: new Transform.rotate(
-                angle: _iconAnim.value * 2 * PI,
-                child: new Icon(widget._isCorrect == true ? Icons.done: Icons.clear, size: _iconAnim.value * 80.0,)
-                ),
+                  angle: _iconAnim.value * 2 * PI,
+                  child: new Icon(
+                    widget._isCorrect == true ? Icons.done : Icons.clear,
+                    size: _iconAnim.value * 80.0,
+                  )),
             ),
             new Padding(
-              padding: new EdgeInsets.only(bottom:  _iconAnim.value * 20.0,),
+              padding: new EdgeInsets.only(
+                bottom: _iconAnim.value * 20.0,
+              ),
             ),
-            new Text(widget._isCorrect == true ? 'Correct': 'Wrong', style: new TextStyle(color: Colors.white, fontSize:  _iconAnim.value * 50.0),)
+            new Text(
+              widget._isCorrect == true ? 'Correct' : 'Wrong',
+              style: new TextStyle(
+                  color: Colors.white, fontSize: _iconAnim.value * 50.0),
+            )
           ],
         ),
       ),
